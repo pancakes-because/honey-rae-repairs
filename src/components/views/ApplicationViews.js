bee46c5d23caa3b6f165b1b0eeaf40716385218d
Diya O'Brien <dbrien.diya@gmail.com>
2022-11-28 16:05:59 -0600

    working on customer views and employee views

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -12,50 +12,18 @@
 // this is supposed to render the <TicketList> component. 
 // we do that with a <Route> component.
 
-import { Outlet, Route, Routes } from "react-router-dom" // don't know what "react-router-dom" is, figure out later
-import { TicketForm } from "../tickets/TicketForm" // importing the "TicketForm" function from "TicketForm.js" inside the "tickets" folder
-import { TicketContainer } from "../tickets/TicketContainer" // importing the "TicketContainer" function from "TicketForm.js" inside the "tickets" folder
+/* *** the outlet, route, routes, ticketform, and ticketcontainer were for the original the new code *** */ 
+// import { Outlet, Route, Routes } from "react-router-dom" 
+// import { TicketForm } from "../tickets/TicketForm" 
+// import { TicketContainer } from "../tickets/TicketContainer" 
 
 // *** don't need to import TicketList and TicketSearch since we're importing TicketCOntainer ***
 // import { TicketList } from "../tickets/TicketList" 
 // import { TicketSearch } from "../tickets/TicketSearch" 
 
-export const ApplicationViews = () => {
-	return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1>Honey Rae Repair Shop</h1>
-                    <div>Your one-stop-shop to get all your electronics fixed</div>
 
-                    <Outlet />
-                </>
-            }>
-
-                {/* original code  */}
-                {/* <Route path="tickets" element={ <TicketList /> } /> */}
-
-                {/* updated code */}
-                {/* this will be the parent component handling both the child TicketList and TicketSearch routes  */}
-
-                {/* <Route path="tickets" element={
-                    <>
-                        <TicketSearch /> 
-                        <TicketList /> 
-                    </>
-                }>  */}
-
-                {/* updated code  */}
-                {/* this route functions the same as above */}
-                {/* TicketContainer is holding TicketSearch and TicketList  */}
-                
-                <Route path="tickets" element={ <TicketContainer /> } /> 
-
-                <Route path="ticket/create" element={ <TicketForm /> } /> 
-            </Route>
-        </Routes>
-    )
-}
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews" 
 
 {/* *** making a new component called "TicketContainer.js" *** */}
 {/* this will make up the parent component that contains TicketSearch and TicketList, so they can share state */}
@@ -66,3 +34,71 @@ export const ApplicationViews = () => {
 {/* customers should only see the TicketList view, so NOT including the search field  */}
 // *** to do this, we're creating two new components to represent two new views *** 
 // *** these will be called "CustomerViews.js" and "Employee Views.js" *** 
+
+/* BIG CHANGE: READ THIS */ 
+// we're going to modify <Outlet> and check if the user is an employee or customer 
+// *** so we're going to drastically change our ApplicationViews code now ***
+// *** so comment this original code out *** 
+// *** look at the updated code block under this next big code blog  ***
+
+/* original ApplicationViews code before CustomerViews.js and EmployeeViews.js were created */ 
+
+// export const ApplicationViews = () => {
+// 	return (
+//         <Routes>
+//             <Route path="/" element={
+//                 <>
+//                     <h1>Honey Rae Repair Shop</h1>
+//                     <div>Your one-stop-shop to get all your electronics fixed</div>
+
+//                     <Outlet />
+//                 </>
+//             }>
+
+//                 {/* original code  */}
+//                 {/* <Route path="tickets" element={ <TicketList /> } /> */}
+
+//                 {/* updated code */}
+//                 {/* this will be the parent component handling both the child TicketList and TicketSearch routes  */}
+
+//                 {/* <Route path="tickets" element={
+//                     <>
+//                         <TicketSearch /> 
+//                         <TicketList /> 
+//                     </>
+//                 }>  */}
+
+//                 {/* updated code  */}
+//                 {/* this route functions the same as above */}
+//                 {/* TicketContainer is holding TicketSearch and TicketList  */}
+                
+//                 <Route path="tickets" element={ <TicketContainer /> } /> 
+
+//                 <Route path="ticket/create" element={ <TicketForm /> } /> 
+//             </Route>
+//         </Routes>
+//     )
+// }
+
+/* updated code after */ 
+
+// so now, with a clean slate, we're going to copy/paste our user and local stroage code from TicketList.js
+/* we have also just imported our CustomerViews and EmployeeViews functions */ 
+/* *** this makes our other imports obsolete now, so we can comment those out as well *** */ 
+
+export const ApplicationViews = () => {
+
+    const localHoneyUser = localStorage.getItem("honey_user") 
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+        if (honeyUserObject.staff) {
+            // Return employee views 
+            return <EmployeeViews /> 
+
+        } 
+        else { 
+            // Return customer views 
+            return <CustomerViews /> 
+
+        }
+}
