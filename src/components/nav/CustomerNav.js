2b9ef95cdec2b410866200814911193ffd99a582
Diya O'Brien <dbrien.diya@gmail.com>
2022-11-28 23:44:26 -0600

    created and finished customer and employee navbars with styling for employee list

---
+++ src/components/nav/CustomerNav.js
@@ -0,0 +1,34 @@
+/* mostly copy/pasted from NavBar.js */ 
+
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+// reminder, we don't want customers to see the "employees" link
+// so we can remove this part 
+
+export const CustomerNav = () => {
+    const navigate = useNavigate()
+
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/tickets">Tickets</Link>
+            </li>
+
+            {/* dont' need this for customers  */}
+            {/* <li className="navbar__item active">
+                <Link className="navbar__link" to="/employees">Employees</Link>
+            </li> */}
+            {
+                localStorage.getItem("honey_user")
+                    ? <li className="navbar__item navbar__logout">
+                        <Link className="navbar__link" to="" onClick={() => {
+                            localStorage.removeItem("honey_user")
+                            navigate("/", {replace: true})
+                        }}>Logout</Link>
+                    </li>
+                    : ""
+            }
+        </ul>
+    )
+}
